870789de9503163f35bad98f3c2f93dd63e80e8c
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-08-12 19:28:52 +1000

    basic shape

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import * as THREE from 'three'
+import lifecycle from 'react-pure-lifecycle'
 
 let _isMounted = false
 
@@ -17,25 +18,25 @@ function App() {
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
     const renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-    const cube = new THREE.Mesh(geometry, material)
-    scene.add(cube)
+    document.getElementById('canvas').appendChild(renderer.domElement)
+    const geometry = new THREE.SphereBufferGeometry(1, 5, 1)
+    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
+    const sphere = new THREE.Mesh(geometry, material)
+    scene.add(sphere)
     camera.position.z = 5
     const animate = function() {
       requestAnimationFrame(animate)
-      cube.rotation.x += 0.01
-      cube.rotation.y += 0.01
+      sphere.rotation.x += 0.01
+      sphere.rotation.y += 0.01
       renderer.render(scene, camera)
     }
     animate()
   })
 
   return (
-    <div className="App">
+    <div id="canvas" className="App">
     </div>
   )
 }
 
-export default App
+export default lifecycle(methods)(App)
