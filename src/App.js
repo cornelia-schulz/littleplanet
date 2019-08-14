3a655a05ad4ff9810af7908034834d973833af71
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-08-15 07:52:39 +1000

    add image to the little planet

--- src/App.js
+++ src/App.js
@@ -15,13 +15,23 @@ function App() {
   useEffect(() => {
     _isMounted = true
     const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+    const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 1000)
+    camera.position.set(-100, 350, 100)
+    camera.lookAt(scene.position)
     const renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setClearColor(0x000000)
+    renderer.setPixelRatio(window.devicePixelRatio || 1)
     document.getElementById('canvas').appendChild(renderer.domElement)
-    const geometry = new THREE.SphereBufferGeometry(1, 5, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0xffff00 })
+    const spotLight = new THREE.SpotLight(0xffffff)
+    spotLight.position.set(-10, 100, 50)
+    scene.add(spotLight)
+    const ambient = new THREE.AmbientLight(0x666666)
+    scene.add(ambient)
+    const geometry = new THREE.SphereBufferGeometry(80, 80, 80)
+    const material = new THREE.MeshPhongMaterial({  map: new THREE.TextureLoader().load('img/Auckland.jpg') })
     const sphere = new THREE.Mesh(geometry, material)
+    sphere.position.set(0, 0, 0)
     scene.add(sphere)
     camera.position.z = 5
     const animate = function() {
